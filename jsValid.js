72fb1271b165e38d79f543ee8f8bff37787fc28f
Juan <juan@cxdmail.com>
2013-11-05 22:04:58 -0800

    Added email validation

--- jsValid.js
+++ jsValid.js
@@ -85,6 +85,18 @@
                 };
 
 
+
+        // Email
+
+            // checks
+            me.isEmailAddress = function(){
+
+                var email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                return me.isNonEmptyString( value ) && email_regex.test( value );
+
+            };
+
     };
 
 
